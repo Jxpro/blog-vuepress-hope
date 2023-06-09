fa5f70b4cba599806c97f4e43d98f60e4d5e3e12
jokerxin <jxpro@qq.com>
2023-06-10 04:28:01 +0800

    Remove home page from timeline list

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -48,10 +48,11 @@ export default hopeTheme({
 
         blog: {
             // 默认情况下，所有从 Markdown 源文件中生成的非主页页面，会被作为文章。
+            // filter默认值：(page) => Boolean(page.filePathRelative) && !page.frontmatter.home
             // 所以由autoCatalog插件生成的目录不会作为文章，这里需要通过filter将其设置为文章，这样上面通过frontmatter添加的star: true才会生效
             // 需要注意的是，设置为文章不意味着会在文章列表中显示，文章列表的过滤使用的是另外的方法
-            // 文具体来说，章列表的显示需要通过frontmatter中的article来控制，必须有对应的markdown文件，且home: false
-            filter: () => true,
+            // 具体来说，文章列表的显示需要通过frontmatter中的article来控制，必须有对应的markdown文件，且home: false
+            filter: page => !page.frontmatter.home,
             // excerpt: false,
         },
 
